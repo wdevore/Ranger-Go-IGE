75be086ddcaa2b056ff38c697fedf8b9a5a906d2
William Cleveland <iposthuman@pop-os.localdomain>
2020-10-16 21:46:44 -0400

    removed unused code

--- examples/basic/a2_raster_text/basic_scene_splash.go
+++ examples/basic/a2_raster_text/basic_scene_splash.go
@@ -24,14 +24,6 @@ func newBasicSplashScene(name string, world api.IWorld) (api.INode, error) {
 func (s *sceneSplash) build(world api.IWorld) error {
 	s.Node.Build(world)
 
-	// dvr := s.World().Properties().Window.DeviceRes
-
-	// bg, err := newBackgroundNode("Background", world, world.Underlay())
-	// if err != nil {
-	// 	return err
-	// }
-	// bg.SetScaleComps(float32(dvr.Width), float32(dvr.Height))
-
 	_, err := newBasicGameLayer("Game Layer", world, s)
 	if err != nil {
 		return err
